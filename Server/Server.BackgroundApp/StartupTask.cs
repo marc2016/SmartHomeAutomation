7bcdad94f257e85746bbd2053572b7c1046bda91
Marc Lammers <lammers.marc@gmail.com>
2017-01-03 09:41:55 +0100

    Background Server Task added.

---
+++ Server/Server.BackgroundApp/StartupTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+using Windows.ApplicationModel.Background;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace Server.BackgroundApp
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        #region Fields
+
+        private BackgroundTaskDeferral deferral;
+
+        #endregion
+
+        #region Method
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            this.deferral = taskInstance.GetDeferral();
+        }
+
+        #endregion
+    }
+}
