9c1b49cad55cf3b47d8162ca9aa0e8887ccd8e1a
Marc Lammers <lammers.marc@gmail.com>
2017-01-04 14:30:18 +0100

    Mobile App project and references added.

--- Server/Server.BackgroundApp/StartupTask.cs
+++ Server/Server.BackgroundApp/StartupTask.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 using Windows.ApplicationModel.Background;
+
+using Sockets.Plugin;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -20,6 +23,27 @@ namespace Server.BackgroundApp
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             this.deferral = taskInstance.GetDeferral();
+            var listenPort = 11000;
+            var listener = new TcpSocketListener();
+
+            // when we get connections, read byte-by-byte from the socket's read stream
+            listener.ConnectionReceived += async (sender, args) =>
+            {
+                var client = args.SocketClient;
+
+                var bytesRead = -1;
+                var buf = new byte[1];
+
+                while (bytesRead != 0)
+                {
+                    bytesRead = await args.SocketClient.ReadStream.ReadAsync(buf, 0, 1);
+                    if (bytesRead > 0)
+                        Debug.Write(buf[0]);
+                }
+            };
+
+            // bind to the listen port across all interfaces
+            listener.StartListeningAsync(listenPort);
         }
 
         #endregion
---
+++ Sha.Client.MobileApp/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sockets.Plugin;
+
+using Xamarin.Forms;
+
+namespace Sha.Client.MobileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NewMethod();
+        }
+
+        private static async Task NewMethod()
+        {
+            var address = "192.168.178.104";
+            var port = 11000;
+            var r = new Random();
+
+            var client = new TcpSocketClient();
+            await client.ConnectAsync(address, port);
+
+            // we're connected!
+            for (int i = 0; i < 5; i++)
+            {
+                // write to the 'WriteStream' property of the socket client to send data
+                var nextByte = (byte)r.Next(0, 254);
+                //client.WriteStream.Write();
+                client.WriteStream.WriteByte(nextByte);
+                await client.WriteStream.FlushAsync();
+
+                // wait a little before sending the next bit of data
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+            }
+
+            await client.DisconnectAsync();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            NewMethod();
+        }
+    }
+}
